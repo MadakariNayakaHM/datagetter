553402f6bc9179521edf2b8d8472ac47ea920a2e
MadakariNayakaHM <mahm20cs@cmrit.ac.in>
2022-09-03 20:44:51 +0530

    changes in data.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv=require('dotenv');
 const { default: mongoose } = require('mongoose');
 dotenv.config({path:'./config.env'})
 const port=process.env.PORT;
-const db=process.env.Database
+const db=process.env.DATABASE
 console.log(process.env.NODE_ENV)
 mongoose.connect(db).then(console.log(`app is running at the port ${port}`)).catch();
 app.listen(port,()=>console.log(`app is running at the port ${port} database connected`))
