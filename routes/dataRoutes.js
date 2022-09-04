74827088d1e93efb7e3b697deffbda24f988949a
MadakariNayakaHM <mahm20cs@cmrit.ac.in>
2022-09-04 17:10:45 +0530

    final trial

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -2,6 +2,6 @@ const express=require('express');
 const dataController=require('./../controller/dataController');
 const Router=express.Router();
 
-Router.route('/').post(dataController.sendData);
+Router.route('/post').post(dataController.sendData);
 
 module.exports=Router;
